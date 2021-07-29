f359744b4e8bc7296793a883ce7027bbec444544
Josef <coreos@JosefdeMac-mini.local>
2021-07-29 17:13:19 +0800

    blogs done

--- www/handlers.py
+++ www/handlers.py
@@ -6,15 +6,26 @@
 __author__ = 'Josef Luo'
 __version__ = 'v0.1'
 
-from models import User, Comment, Blog, next_id
+from models import User, Comment, Blog, next_id, time
 from corweb import get, post
 import asyncio
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
+    blogs = [
+         Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
+         Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
+         Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+    ]
     return {
-        '__template__': 'test.html',
-        'users': users
+        '__template__': 'blogs.html',
+        'blogs': blogs
     }
 
+@get('/api/users')
+async def api_get_users():
+    users = await User.findAll(orderBy='created_at DESC')
+    for u in users:
+        u.password = '******'
+    return dict(users=users)
